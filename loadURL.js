5cf3331c3f0237a18a78ee02375773242605a0af
fulelgupport <roman.c@aloteq.com>
2021-10-04 14:02:41 +0300

    upd

---
+++ loadURL.js
@@ -0,0 +1,53 @@
+async function post(url, api_key, payload) {
+    let result;
+
+    try {
+        result = await $.ajax({
+            url: url,
+            type: 'POST',
+            headers: { 'x-api-key': api_key, 'content-type': 'application/json' },
+            data: JSON.stringify(payload)
+        });
+
+        return result;
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+async function call_api() {
+  let url = "https://m.api.lura.app/play/v1/media/session";
+  let api_key = "hyD9ACaFYe3hpyWWRuOPzkjgrkwX6aFOLIPmpZfeyqn4oirAUybejNkXRhZes23DnIJx77RKmtswSqpIWpQGVO";
+  let data = {
+    "ad_url": "https://storage.googleapis.com/m300732-mcp-media/vast3.xml",
+    "manifest_url": "mcp://channel/1477/manifest.m3u8",
+    "fields": ["timeline", "tracking"],
+    "developer_options": {
+      "enabled": true,
+      "adops_id": "Qtge28wiFFgCsLq5"
+    }
+  };
+
+  let result = await post ( url, api_key, data );
+  if ( result && result.session_id) {
+    return result;
+  } else {
+    return null;
+  }
+}
+
+async function load_player() {
+  let start = Date.now();
+  let api_response = await call_api();
+  console.log(Date.now() - start, api_response);
+
+  if ( !api_response.stitched_url ) {
+    console.log("Response", api_response);
+    $('#messages').text("Cannot access this content. Check your authorization settings.");
+    return;
+  }
+
+  window.url = api_response.stitched_url;
+}
+
+load_player();
